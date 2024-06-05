c3f588bd59469c938080b8400fbaffad6655fbaf
phung hoang <lnphunghoang190903@gmail.com>
2024-06-05 10:52:52 +0700

    Homepage Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,12 +5,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='mt-9 flex flex-col gap-9'>
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className='flex flex-col gap-5'>
-        <h1 className='text-20 font-bolg text-white-1'>Trending Podcasts</h1>
-        <div className='podcast_gird'>
-          {podcastData.map(({id, title, description, imgURL}) => (
-            <PodcastCard 
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+
+        <div className="podcast_grid">
+          {podcastData.map(({ id, title, description, imgURL }) => (
+            <PodcastCard
               key={id}
               imgUrl={imgURL}
               title={title}
