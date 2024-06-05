cb0a886a6ee46cb51dfc79aa05176336236c6e68
phung hoang <lnphunghoang190903@gmail.com>
2024-06-05 10:47:11 +0700

    Update PodcastCard

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -18,6 +18,11 @@ const PodcastCard = ({
                 height={174}
                 alt={title} 
             />
+
+            <div className='flex flex-col'>
+                <h1 className='text-16 truncate font-bold text-white-1'>{title}</h1>
+                <h2 className='text-12 truncate font-normal capitalize text-white-4'>{description}</h2>
+            </div>
         </figure>
     </div>
   )
