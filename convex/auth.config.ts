0aa9058d649b8e448fea642a175fb1bc487755f8
phung hoang <lnphunghoang190903@gmail.com>
2024-06-13 09:30:40 +0700

    Clerk Setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig =  {
+    providers: [
+      {
+        domain: "https://fine-mollusk-77.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authConfig;
